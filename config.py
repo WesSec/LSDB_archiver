59e34d0dc47ae8b10b480961f85197aa3de96ad3
Niels Dunnewind <n.dunnewind@erasmusmc.nl>
2019-09-17 21:33:13 +0200

    Change tabs to spaces

--- config.py
+++ config.py
@@ -6,13 +6,13 @@ parser = argparse.ArgumentParser()
 
 
 def loadargs():
-	parser.add_argument("-i", "--ID", help="Enter a set ID to download")
-	parser.add_argument("-l", "--list",
-	                    help="Location of a list (txt, every ID on a new row) with setlists to download")
-	parser.add_argument("-o", "--output", help="Download location eg: /home/user/livesets (note, do not add a slash at "
-	                                           "the end default == project folder")
-	args = parser.parse_args()
-	return args
+    parser.add_argument("-i", "--ID", help="Enter a set ID to download")
+    parser.add_argument("-l", "--list",
+                        help="Location of a list (txt, every ID on a new row) with setlists to download")
+    parser.add_argument("-o", "--output", help="Download location eg: /home/user/livesets (note, do not add a slash at "
+                                               "the end default == project folder")
+    args = parser.parse_args()
+    return args
 
 
 prog_path = os.path.dirname(os.path.abspath(__file__))
@@ -20,6 +20,6 @@ prog_path = os.path.dirname(os.path.abspath(__file__))
 
 # Import the config file
 def Load_config():
-	config = configparser.RawConfigParser(allow_no_value=True)
-	config.read(prog_path + "/config.ini")
-	return config
+    config = configparser.RawConfigParser(allow_no_value=True)
+    config.read(prog_path + "/config.ini")
+    return config
--- main.py
+++ main.py
@@ -14,132 +14,132 @@ prog_path = os.path.dirname(os.path.abspath(__file__))
 
 
 def GetMetadata(soup):
-	# Retrieve Artist name
-	metadata = {}
-	artists = soup.select("a[href*=artist]")
-	# Retrieve Event name
-	event = soup.select_one("a[href*=event]").text
-	# Combine artist names if b2b set
-	artist = ' & '.join(rat.text for rat in artists)
-	# Write data into dict
-	metadata.update({'Artist': artist})
-	metadata.update({'Event': event})
-	# combine full filename
-	return metadata
+    # Retrieve Artist name
+    metadata = {}
+    artists = soup.select("a[href*=artist]")
+    # Retrieve Event name
+    event = soup.select_one("a[href*=event]").text
+    # Combine artist names if b2b set
+    artist = ' & '.join(rat.text for rat in artists)
+    # Write data into dict
+    metadata.update({'Artist': artist})
+    metadata.update({'Event': event})
+    # combine full filename
+    return metadata
 
 
 # Get setdata from LSDB and check for availability
 def DownloadSet(setID):
-	result = requests.get("https://lsdb.nl/set/" + setID)
-	if result.status_code == 200:
-		soup = BeautifulSoup(result.content, "html.parser")
-		# Check if youtube/mixcloud/soundcloud are available
-		reg = re.compile(r'you|mixcloud|soundcloud')
-		golink = [e for e in soup.find_all('a') if reg.match(e.text)]
-		metadata = GetMetadata(soup)
-		# If youtube/mixcloud/soundcloud is available, letsgodownload
-		if golink:
-			FollowDownloadLink("https://lsdb.nl" + golink[0]['href'], 'ytdl', metadata)
-		# If not, try to download using archive.org
-		else:
-			reg = re.compile(r'archive.org')
-			golink = [e for e in soup.find_all('a') if reg.match(e.text)]
-			if golink:
-				FollowDownloadLink("https://lsdb.nl" + golink[0]['href'], 'direct', metadata)
-			else:
-				reg = re.compile(r'zippy')
-				golink = [e for e in soup.find_all('a') if reg.match(e.text)]
-				if golink:
-					print("Only a zippy link found, download manual here: " + "https://lsdb.nl" + golink[0]['href'],
-					      metadata)
-				else:
-					print("No download found, i'm sorry :(, set ID: " + setID)
-					return
+    result = requests.get("https://lsdb.nl/set/" + setID)
+    if result.status_code == 200:
+        soup = BeautifulSoup(result.content, "html.parser")
+        # Check if youtube/mixcloud/soundcloud are available
+        reg = re.compile(r'you|mixcloud|soundcloud')
+        golink = [e for e in soup.find_all('a') if reg.match(e.text)]
+        metadata = GetMetadata(soup)
+        # If youtube/mixcloud/soundcloud is available, letsgodownload
+        if golink:
+            FollowDownloadLink("https://lsdb.nl" + golink[0]['href'], 'ytdl', metadata)
+        # If not, try to download using archive.org
+        else:
+            reg = re.compile(r'archive.org')
+            golink = [e for e in soup.find_all('a') if reg.match(e.text)]
+            if golink:
+                FollowDownloadLink("https://lsdb.nl" + golink[0]['href'], 'direct', metadata)
+            else:
+                reg = re.compile(r'zippy')
+                golink = [e for e in soup.find_all('a') if reg.match(e.text)]
+                if golink:
+                    print("Only a zippy link found, download manual here: " + "https://lsdb.nl" + golink[0]['href'],
+                          metadata)
+                else:
+                    print("No download found, i'm sorry :(, set ID: " + setID)
+                    return
 
-	else:
-		print("Something went wrong while looking up download link. exiting..")
-		sys.exit()
+    else:
+        print("Something went wrong while looking up download link. exiting..")
+        sys.exit()
 
 
 # Check go URL and get actual link
 def FollowDownloadLink(go_url, provider, metadata):
-	# Actual link is behind a referral url.
-	result = requests.get(go_url)
-	if result.status_code == 200:
-		soup = BeautifulSoup(result.content, "html.parser")
-		# Get actual download link
-		downloadlink = soup.find_all("a")
-		# Debug purposes
-		print(downloadlink[1].string)
-		# Create event directory if not existing
-		MakeDir(metadata['Event'])
-		# Actual download command (zippy not supported at this moment
-		if provider == 'ytdl':
-			ytdownload(downloadlink[1].string, metadata)
-		elif provider == 'direct':
-			directdownload(downloadlink[1].string, metadata)
-	else:
-		print("Something went wrong. exiting program")
-		sys.exit()
+    # Actual link is behind a referral url.
+    result = requests.get(go_url)
+    if result.status_code == 200:
+        soup = BeautifulSoup(result.content, "html.parser")
+        # Get actual download link
+        downloadlink = soup.find_all("a")
+        # Debug purposes
+        print(downloadlink[1].string)
+        # Create event directory if not existing
+        MakeDir(metadata['Event'])
+        # Actual download command (zippy not supported at this moment
+        if provider == 'ytdl':
+            ytdownload(downloadlink[1].string, metadata)
+        elif provider == 'direct':
+            directdownload(downloadlink[1].string, metadata)
+    else:
+        print("Something went wrong. exiting program")
+        sys.exit()
 
 
 # checks if event directory exists, othewise creates it
 def MakeDir(event):
-	if not os.path.exists(output + "/" + event):
-		os.makedirs(output + "/" + event)
+    if not os.path.exists(output + "/" + event):
+        os.makedirs(output + "/" + event)
 
 
 # Used for archive.org
 def directdownload(link, metadata):
-	print("start download using direct download")
-	if link.find('/'):
-		ext = link.rsplit('.', 1)[1]
-		r = requests.get(link)
-		with open(output + '/' + metadata['Event'] + '/' + metadata['Artist'] + " @ " + metadata['Event'] + "." + ext,
-		          'wb') as f:
-			f.write(r.content)
-			print("download finished: " + metadata['Artist'] + " @ " + metadata['Event'])
+    print("start download using direct download")
+    if link.find('/'):
+        ext = link.rsplit('.', 1)[1]
+        r = requests.get(link)
+        with open(output + '/' + metadata['Event'] + '/' + metadata['Artist'] + " @ " + metadata['Event'] + "." + ext,
+                  'wb') as f:
+            f.write(r.content)
+            print("download finished: " + metadata['Artist'] + " @ " + metadata['Event'])
 
 
 def ytdownload(link, metadata):
-	download_location = output + '/' + metadata['Event'] + '/' + metadata['Artist'] + " @ " + metadata[
-		'Event'] + '.%(ext)s'
-	# Check config file if mp3 is needed, otherwise download mp3/m4a. Note: mp3 conversion make the scrape slow as fuck.
-	if config.getboolean('DEFAULT', 'force_mp3'):
-		ydl_opts = {'outtmpl': download_location, 'format': 'bestaudio/best', 'quiet': 'true',
-		            'postprocessors': [{
-			            'key': 'FFmpegExtractAudio',
-			            'preferredcodec': 'mp3',
-		            }], }
-	else:
-		ydl_opts = {'outtmpl': download_location, 'quiet': 'true', 'format': 'bestaudio/best', }
-	# Download the shit
-	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-		ydl.download([link])
-		print(download_location)
+    download_location = output + '/' + metadata['Event'] + '/' + metadata['Artist'] + " @ " + metadata[
+        'Event'] + '.%(ext)s'
+    # Check config file if mp3 is needed, otherwise download mp3/m4a. Note: mp3 conversion make the scrape slow as fuck.
+    if config.getboolean('DEFAULT', 'force_mp3'):
+        ydl_opts = {'outtmpl': download_location, 'format': 'bestaudio/best', 'quiet': 'true',
+                    'postprocessors': [{
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': 'mp3',
+                    }], }
+    else:
+        ydl_opts = {'outtmpl': download_location, 'quiet': 'true', 'format': 'bestaudio/best', }
+    # Download the shit
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
+        print(download_location)
 
 
 if __name__ == '__main__':
-	# Load arguments
-	args = config.loadargs()
-	config = config.Load_config()
-	# If custom location is set, use it
-	if args.output:
-		output = args.output
-	# If no custom location is set: use project directory
-	else:
-		output = prog_path
-	# If ID is given, try to download the specific ID, error if something goes wrong
-	if args.ID:
-		DownloadSet(args.ID)
-	# If list with ID's is provided, loop through file and try download for each setID
-	if args.list:
-		try:
-			f = open(args.list, "r")
-			for line in f:
-				try:
-					DownloadSet(line)
-				except:
-					print("something went wrong with setID: " + line)
-		except:
-			print("task failed successfully")
+    # Load arguments
+    args = config.loadargs()
+    config = config.Load_config()
+    # If custom location is set, use it
+    if args.output:
+        output = args.output
+    # If no custom location is set: use project directory
+    else:
+        output = prog_path
+    # If ID is given, try to download the specific ID, error if something goes wrong
+    if args.ID:
+        DownloadSet(args.ID)
+    # If list with ID's is provided, loop through file and try download for each setID
+    if args.list:
+        try:
+            f = open(args.list, "r")
+            for line in f:
+                try:
+                    DownloadSet(line)
+                except:
+                    print("something went wrong with setID: " + line)
+        except:
+            print("task failed successfully")
